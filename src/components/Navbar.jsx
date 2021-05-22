44a80cc9793f9911c41c92f6261dbdefa1d0a4bd
TaKuYa YaNaSe <yt.baseball1168@gmail.com>
2021-05-23 01:29:13 +0900

    ナビゲーションバーを修正

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ function NavBar() {
   return (
     <div>
       <nav className={nav ? "nav active" : "nav"}>
-        <Link to="main" className="logo">
+        <Link to="main" className="logo" smooth={true} duration={2000}>
           <img src={logo} alt="" />
         </Link>
         <input className="menu-btn" type="checkbox" id={"menu-btn"} />
@@ -27,16 +27,24 @@ function NavBar() {
         </label>
         <ul className="menu">
           <li>
-            <Link to="#">Header</Link>
+            <Link to="main" smooth={true} duration={2000}>
+              Header
+            </Link>
           </li>
           <li>
-            <Link to="#">Products</Link>
+            <Link to="products" smooth={true} duration={2000}>
+              Products
+            </Link>
           </li>
           <li>
-            <Link to="#">About</Link>
+            <Link to="about" smooth={true} duration={2000}>
+              About
+            </Link>
           </li>
           <li>
-            <Link to="#">Contacts</Link>
+            <Link to="contact" smooth={true} duration={2000}>
+              Contacts
+            </Link>
           </li>
         </ul>
       </nav>
