7d2c005dbed1c371809a4d70934d4e3e22b9d203
TaKuYa YaNaSe <yt.baseball1168@gmail.com>
2021-05-22 23:24:09 +0900

    ナビゲーションメニューを追加

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import logo from "../images/logo1.png";
 
 function NavBar() {
-  return <div></div>;
+  const [nav, setNav] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 50) {
+      setNav(true);
+    } else {
+      setNav(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBackground);
+
+  return (
+    <div>
+      <nav className={nav ? "nav active" : "nav"}>
+        <Link to={"main"} className="logo">
+          <img src={logo} alt="" />
+        </Link>
+        <input className="menu-btn" id={"menu-btn"} />
+        <label className="menu-icon" for="menu-btn">
+          <span className="nav-icon"></span>
+        </label>
+        <ul className="menu">
+          <li>
+            <Link to="#">Heder</Link>
+          </li>
+          <li>
+            <Link to="#">Products</Link>
+          </li>
+          <li>
+            <Link to="#">About</Link>
+          </li>
+          <li>
+            <Link to="#">Contacts</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 export default NavBar;
